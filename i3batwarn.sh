2e31aa2aa1a07c14b5834f9edbe16a71543c6740
Aaro Salosensaari <a.salosensaari@gmail.com>
2017-04-17 22:59:22 +0300

    customizations

--- i3batwarn.sh
+++ i3batwarn.sh
@@ -8,20 +8,20 @@
 #############################################
 
 # lock file location
-export LOCK_FILE=/tmp/battery_state.lock
+export BATT_LOCK_FILE=/tmp/battery_state.lock
 
 # check if another copy is running
-if [[ -a $LOCK_FILE ]]; then
+if [[ -a $BATT_LOCK_FILE ]]; then
 
-    pid=$(cat $LOCK_FILE | awk '{print $1}')
-	ppid=$(cat $LOCK_FILE | awk '{print $2}')
+    pid=$(cat $BATT_LOCK_FILE | awk '{print $1}')
+	ppid=$(cat $BATT_LOCK_FILE | awk '{print $2}')
 	# validate contents of previous lock file
 	vpid=${pid:-"0"}
 	vppid=${ppid:-"0"}
 
     if (( $vpid < 2 || $vppid < 2 )); then
-		# corrupt lock file $LOCK_FILE ... Exiting
-		cp -f $LOCK_FILE ${LOCK_FILE}.`date +%Y%m%d%H%M%S`
+		# corrupt lock file $BATT_LOCK_FILE ... Exiting
+		cp -f $BATT_LOCK_FILE ${BATT_LOCK_FILE}.`date +%Y%m%d%H%M%S`
 		exit
 	fi
 
@@ -33,13 +33,13 @@ if [[ -a $LOCK_FILE ]]; then
 		exit
 	else
 		# bogus lock file found, removing
-		rm -f $LOCK_FILE >/dev/null
+		rm -f $BATT_LOCK_FILE >/dev/null
 	fi
 
 fi
 
 pid=$$
-ps -f -p $pid --no-headers | awk '{print $2,$3}' > $LOCK_FILE
+ps -f -p $pid --no-headers | awk '{print $2,$3}' > $BATT_LOCK_FILE
 # starting with process id $pid
 
 # set Battery
@@ -61,10 +61,11 @@ FULL=`grep "POWER_SUPPLY_ENERGY_FULL_DESIGN" $ACPI_PATH/uevent | cut -d= -f2`
 PERCENT=`echo $(( $REM * 100 / $FULL ))`
 
 # set error message
-MESSAGE="AWW SNAP! I am running out of juice ...  Please, charge me or I'll have to power down."
+MESSAGE="DANGER, WILL ROBINSON! LOW BATTERY!"
 
 # set energy limit in percent, where warning should be displayed
-LIMIT="10"
+#LIMIT="10"
+LIMIT="7"
 
 # show warning if energy limit in percent is less then user set limit and
 # if battery is discharging
